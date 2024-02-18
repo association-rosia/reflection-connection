a8abb12baf02cd7d96f5b30bd115bc090dd1a42f
Louis <louis.reberga@gmail.com>
2024-02-19 00:45:24 +0100

    feat: Submissions pipeline

---
+++ src/submissions/make_submissions.py
@@ -0,0 +1,85 @@
+import json
+import numpy as np
+import src.submissions.inference_model as im
+from src.submissions import search
+from src import utils
+import os
+
+
+def main():
+    config = utils.get_config()
+    wandb_run = utils.get_run('2khs9u4f')
+    query_set = search.ImageSet(config, wandb_run, query=True, cuda_idx=0)
+    corpus_set = search.ImageSet(config, wandb_run, query=False, cuda_idx=0)
+    query_set.build_embeddings()
+    corpus_set.build_embeddings()
+    sbf = search.SearchBruteForce(corpus_set)
+    query_image_labels, distances, _, matched_labels = sbf.query(query_set)
+    confidence_scores = dist_to_conf(distances)
+    result_builder = ResultBuilder(config)
+    result_builder = result_builder(
+        query_image_labels,
+        matched_labels,
+        confidence_scores,
+        f'{wandb_run.name}-{wandb_run.id}'
+    )
+
+def dist_to_conf(distances: np.ndarray):
+    max_dist = distances.max(axis=1).reshape(-1, 1)
+    min_dist = distances.min(axis=1).reshape(-1, 1)
+
+    distances_normalized = (distances - min_dist)/max_dist
+    confidence_from_dist = 1 - distances_normalized
+    
+    return confidence_from_dist
+
+
+class ResultBuilder:
+    def __init__(self, config):
+        self.results = dict()
+        path = config['path']['submissions']['root']
+        self.path = utils.get_notebooks_path(path)
+        
+    def build(self, 
+              query_image_labels: np.ndarray, 
+              matched_labels: np.ndarray,   
+              confidence_scores: np.ndarray):
+            
+        # validate shapes of inputs
+        if len(query_image_labels.shape) != 1:
+            raise ValueError(f'Expected query_image_labels to be 1-dimensional array, got {query_image_labels.shape} instead')
+        
+        if matched_labels.shape != (query_image_labels.shape[0],3):
+            raise ValueError(f'Expected matched_labels to have shape {(query_image_labels.shape[0], 3)}, got {matched_labels.shape} instead')
+        
+        if confidence_scores.shape != (query_image_labels.shape[0],3):
+            raise ValueError(f'Expected confidence_scores to have shape {(query_image_labels.shape[0], 3)}, got {confidence_scores.shape} instead')
+            
+        for i, x in enumerate(query_image_labels):
+            labels = matched_labels[i]
+            confidence = confidence_scores[i]
+    
+            result_x = [{'label': labels[j], 'confidence': confidence[j]} for j in range(0,3)]
+    
+            self.results.update({x: result_x})
+        
+        return self
+    
+    def to_json(self, json_name: str = 'results') -> None:
+        
+        path = os.path.join(self.path, f'{json_name}.json')
+        with open(path, 'w+') as f:
+            json.dump(self.results, f)
+    
+    def __call__(self,
+              query_image_labels: np.ndarray, 
+              matched_labels: np.ndarray,   
+              confidence_scores: np.ndarray,
+              json_name: str = 'results') -> None:
+        
+        self.build(query_image_labels, matched_labels, confidence_scores)
+        self.to_json(json_name)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/submissions/search.py
@@ -0,0 +1,111 @@
+import os
+import torch
+from typing import Any
+import numpy as np
+import wandb.apis.public as wandb_api
+import src.submissions.inference_model as im
+from src import utils
+from PIL import Image
+from torchvision.datasets.folder import default_loader
+from sklearn.metrics.pairwise import pairwise_distances
+from tqdm.autonotebook import tqdm
+
+
+class ImageSet:
+    def __init__(self,
+                 config: dict,
+                 wandb_run: wandb_api.Run | utils.RunDemo,
+                 query: bool,
+                 cuda_idx: int = 0,
+                 ) -> None:
+        self.config = config
+        self.wandb_run = wandb_run
+        self.cuda_idx = cuda_idx
+        self.query = query
+        self.embeddings = None
+
+        if query:
+            path = os.path.join(config['path']['data']['raw']['test'], 'query')
+        else:
+            path = os.path.join(config['path']['data']['raw']['test'], 'image_corpus')
+
+        self.path = utils.get_notebooks_path(path)
+        self.names = os.listdir(self.path)
+
+    def _load_model(self):
+        return im.RefConInferenceModel(self.config, self.wandb_run, self.cuda_idx)
+    
+    def build_embeddings(self):
+        model = self._load_model()
+        self.embeddings = []
+        for img, name in tqdm(self):
+            embedding = model(img)
+            self.embeddings.append((embedding, name))
+        
+        del model
+        torch.cuda.empty_cache()
+        
+        return self
+        
+    def get_embeddings(self) -> list[tuple[torch.Tensor, str]]:
+        if self.embeddings is None:
+            raise RuntimeError('Embedding collection is empty. Run self.build_embeddings() method to build it')
+        
+        return self.embeddings
+    
+    def __len__(self):
+        return len(self.names)
+        
+    def __getitem__(self, index: int) -> tuple[Image.Image, str]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+
+        name = self.names[index]
+        img_path = os.path.join(self.path, name)
+        img = default_loader(img_path)
+
+        return img, name
+
+
+class SearchBruteForce:
+    def __init__(self, corpus_set: ImageSet) -> None:
+        embeddings = corpus_set.get_embeddings()
+        self.corpus_embedings = np.stack([embedding[0].numpy(force=True) for embedding in embeddings])
+        self.corpus_labels = np.stack([embedding[1] for embedding in embeddings])
+        
+    def query(self, query_set: ImageSet, metric: str = 'l2', k: int = 3) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        embeddings = query_set.get_embeddings()
+        query_embedings = np.stack([embedding[0].numpy(force=True) for embedding in embeddings])
+        query_labels = np.stack([embedding[1] for embedding in embeddings])
+        
+        matrix_distances = pairwise_distances(query_embedings, self.corpus_embedings, metric=metric, n_jobs=-1)
+        indices = []
+        distances = []
+        for query_distances in matrix_distances:
+            indices.append(np.argsort(query_distances)[:k])
+            distances.append(query_distances[indices[-1]])
+        
+        distances = np.stack(distances)
+        indices = np.stack(indices)
+        
+        return query_labels, distances, self.corpus_embedings[indices], self.corpus_labels[indices]
+    
+    def __call__(self, *args, **kwargs) -> Any:
+        return self.query(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    config = utils.get_config()
+    wandb_run = utils.get_run('2khs9u4f')
+
+    query_set = ImageSet(config, wandb_run, query=True, cuda_idx=0)
+    corpus_set = ImageSet(config, wandb_run, query=False, cuda_idx=0)
+    query_set.build_embeddings()
+    corpus_set.build_embeddings()
+    sbf = SearchBruteForce(corpus_set)
+    sbf.query(query_set)
