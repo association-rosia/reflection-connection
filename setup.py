5719bfe623a3bcd103799247369dd2c2fc4fecaf
BaptisteUrgell <baptiste.u@gmail.com>
2024-02-18 09:55:01 +0100

    chore: first commit with some basic elements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reflection-connection',
+    version='1.0',
+    author='RosIA',
+    author_email='rosialab31@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # listez vos dépendances ici
+    ],
+)
