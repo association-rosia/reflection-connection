53796400964f4af034116edb2901876794253258
louisreberga <louis.reberga@gmail.com>
2024-03-07 17:44:14 +0000

    Refactored pretrain data extraction process and added functionality to split tiles into train and validation sets.

--- src/data/make_pretrain_data.py
+++ src/data/make_pretrain_data.py
@@ -7,8 +7,11 @@ import cv2
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
+import shutil
 
 import src.utils as utils
+
+from sklearn.model_selection import train_test_split
 
 
 def get_values_counts(config):
@@ -66,11 +69,8 @@ def get_tiles_coords(values, counts, num_tiles=15, max_h=1259, max_w=300):
     return tiles_coords
 
 
-def extract_tiles_from_slice(slice, volume_path, dim, slice_idx, values, counts):
-    save_pretrain_path = os.path.join(config['path']['data'], 'processed', 'pretrain')
+def extract_tiles_from_slice(slice, save_volume_path, dim, slice_idx, values, counts):
     tiles_coords = get_tiles_coords(values, counts)
-    volume_name = volume_path.split('/')[-1].split('.')[0]
-    save_volume_path = os.path.join(save_pretrain_path, volume_name)
     os.makedirs(save_volume_path, exist_ok=True)
 
     for i, (x0, x1, y0, y1) in enumerate(tiles_coords):
@@ -81,25 +81,54 @@ def extract_tiles_from_slice(slice, volume_path, dim, slice_idx, values, counts)
         image.save(save_image_path)
 
 
+def split_tiles(save_volume_path, volume_name):
+    save_train_path = os.path.join(config['path']['data'], 'processed', 'pretrain', 'train')
+    save_val_path = os.path.join(config['path']['data'], 'processed', 'pretrain', 'val', volume_name)
+    os.makedirs(save_val_path, exist_ok=True)
+
+    tile_files = os.listdir(save_volume_path)
+    _, tile_files_val = train_test_split(tile_files, test_size=0.01, random_state=42)
+
+    for tile_file in tile_files_val:
+        tile_path = os.path.join(save_volume_path, tile_file)
+        new_tile_path = os.path.join(save_val_path, tile_file)
+        shutil.move(tile_path, new_tile_path)
+
+    shutil.move(save_volume_path, save_train_path)
+
+
 def extract_tiles_from_volumes(config):
     values, counts = get_values_counts(config)
     data_pretrain_path = os.path.join(config['path']['data'], 'raw', 'pretrain')
+    save_pretrain_path = os.path.join(config['path']['data'], 'processed', 'pretrain')
     data_pretrain_glob = os.path.join(data_pretrain_path, '**/*.npy')
 
     for volume_path in tqdm(glob(data_pretrain_glob, recursive=True)):
         volume = np.load(volume_path)
 
+        volume_name = volume_path.split('/')[-1].replace('.npy', '')
+        save_volume_path = os.path.join(save_pretrain_path, volume_name)
+
         for slice_idx in range(len(volume)):
             slice = volume[slice_idx, :, :].T
-            extract_tiles_from_slice(slice, volume_path, 0, slice_idx, values, counts)
+            extract_tiles_from_slice(slice, save_volume_path, 0, slice_idx, values, counts)
             slice = volume[:, slice_idx, :].T
-            extract_tiles_from_slice(slice, volume_path, 1, slice_idx, values, counts)
+            extract_tiles_from_slice(slice, save_volume_path, 1, slice_idx, values, counts)
 
-        sys.exit(0)
+        split_tiles(save_volume_path, volume_name)
+        # sys.exit(0)
+
+
+def init_folders(config):
+    pretrain_path = os.path.join(config['path']['data'], 'processed', 'pretrain')
+    os.makedirs(pretrain_path, exist_ok=True)
+    pretrain_train_path = os.path.join(config['path']['data'], 'processed', 'pretrain', 'train')
+    os.makedirs(pretrain_train_path, exist_ok=True)
+    pretrain_val_path = os.path.join(config['path']['data'], 'processed', 'pretrain', 'val')
+    os.makedirs(pretrain_val_path, exist_ok=True)
 
 
 if __name__ == "__main__":
     config = utils.get_config()
-    save_pretrain_path = os.path.join(config['path']['data'], 'processed', 'pretrain')
-    os.makedirs(save_pretrain_path, exist_ok=True)
+    init_folders(config)
     extract_tiles_from_volumes(config)
