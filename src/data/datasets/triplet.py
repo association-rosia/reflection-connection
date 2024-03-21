316cc63541c97ac75933790eaf2ede86b99fb030
baptiste-urgell <baptiste.urgell@aqsone.com>
2024-03-21 22:30:56 +0000

    debug merging directly on main

--- src/data/datasets/triplet.py
+++ src/data/datasets/triplet.py
@@ -1,8 +1,5 @@
-import os
-
 import numpy as np
 from PIL import Image
-from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
 
 import src.data.transforms as dT
@@ -18,19 +15,18 @@ class RefConTripletDataset(Dataset):
 
     def __init__(self,
                  wandb_config: dict,
-                 list_class_name: list,
-                 list_img_path: list,
+                 image_paths: list,
+                 labels: list,
                  processor: dT.RefConfProcessor,
                  train: bool
                  ):
         self.wandb_config = wandb_config
-        self.dict_path_img = self._make_dict_path_img(list_class_name, list_img_path)
-        self.set_class_name = set(list_class_name)
+        self.dict_path_img = self._make_dict_path_img(image_paths, labels)
+        self.set_class_name = set(labels)
         self.processor = processor
         self.train = train
-        self.targets, self.img_paths = self._remove_targets(list_class_name, list_img_path)
+        self.image_paths, self.targets = self._remove_targets(image_paths, labels)
         
-
         if not self.train:
             self.triplets = self._generate_triplets()
 
@@ -38,19 +34,19 @@ class RefConTripletDataset(Dataset):
         return len(self.targets)
 
     @staticmethod
-    def _make_dict_path_img(list_class_name, img_paths):
-        dict_path_img = {class_name: [] for class_name in set(list_class_name)}
-        for class_name, path_img in zip(list_class_name, img_paths):
-            dict_path_img[class_name].append(path_img)
+    def _make_dict_path_img(image_paths, labels):
+        dict_image_paths = {label: [] for label in set(labels)}
+        for image_path, label in zip(image_paths, labels):
+            dict_image_paths[label].append(image_path)
 
-        return {k: np.asarray(v) for k, v in dict_path_img.items()}
+        return {k: np.asarray(v) for k, v in dict_image_paths.items()}
 
-    def _remove_targets(self, targets, img_paths):
-        targets = np.asarray(targets)
-        img_paths = np.asarray(img_paths)
+    def _remove_targets(self, image_paths, labels):
+        targets = np.asarray(labels)
+        image_paths = np.asarray(image_paths)
         mask = np.isin(targets, self.wandb_config['exclude_anchor'], invert=True)
 
-        return targets[mask], img_paths[mask]
+        return image_paths[mask], targets[mask]
 
     def _load_image(self, path):
         with open(path, 'rb') as f:
@@ -66,7 +62,7 @@ class RefConTripletDataset(Dataset):
         else:
             return random_state
 
-    def _get_positive_img_path(self, anchor_name, anchor_img_path, random_state=None):
+    def _get_positive_image_path(self, anchor_name, anchor_img_path, random_state=None):
         random_state = self._get_random_state(random_state)
         positive_img_path = anchor_img_path
         while positive_img_path == anchor_img_path:
@@ -74,7 +70,7 @@ class RefConTripletDataset(Dataset):
 
         return positive_img_path
 
-    def _get_negative_img_path(self, anchor_name, random_state=None):
+    def _get_negative_image_path(self, anchor_name, random_state=None):
         random_state = self._get_random_state(random_state)
         possible_classes = np.fromiter(self.set_class_name - {anchor_name}, dtype=object)
         negative_name = random_state.choice(possible_classes)
@@ -84,19 +80,19 @@ class RefConTripletDataset(Dataset):
     def _generate_triplets(self):
         random_state = np.random.RandomState(self.wandb_config['random_state'])
         triplets = []
-        for anchor_img_path, anchor_name in zip(self.img_paths, self.targets):
-            positive_img_path = self._get_positive_img_path(anchor_name, anchor_img_path, random_state)
-            negative_img_path = self._get_negative_img_path(anchor_name, random_state)
+        for anchor_img_path, anchor_name in zip(self.image_paths, self.targets):
+            positive_img_path = self._get_positive_image_path(anchor_name, anchor_img_path, random_state)
+            negative_img_path = self._get_negative_image_path(anchor_name, random_state)
             triplets.append((anchor_img_path, positive_img_path, negative_img_path))
 
         return triplets
 
     def __getitem__(self, idx):
         if self.train:
-            anchor_img_path = self.img_paths[idx]
+            anchor_img_path = self.image_paths[idx]
             anchor_name = self.targets[idx]
-            positive_img_path = self._get_positive_img_path(anchor_name, anchor_img_path)
-            negative_img_path = self._get_negative_img_path(anchor_name)
+            positive_img_path = self._get_positive_image_path(anchor_name, anchor_img_path)
+            negative_img_path = self._get_negative_image_path(anchor_name)
         else:
             anchor_img_path = self.triplets[idx][0]
             positive_img_path = self.triplets[idx][1]
@@ -109,51 +105,11 @@ class RefConTripletDataset(Dataset):
         return anchor_img, positive_img, negative_img
 
 
-def get_class_path(dir_path):
-    list_class_name = []
-    list_img_path = []
-    for class_name in os.listdir(dir_path):
-        class_path = os.path.join(dir_path, class_name)
-        if os.path.isdir(class_path):
-            for img_name in os.listdir(class_path):
-                if img_name.endswith('.png'):
-                    img_path = os.path.join(class_path, img_name)
-                    list_class_name.append(class_name)
-                    list_img_path.append(img_path)
-
-    return list_class_name, list_img_path
-
-
-def get_train_val_split(wandb_config, list_class_name, list_img_path):
-    train_class_name, val_class_name, train_path_img, val_path_img = train_test_split(
-        list_class_name, list_img_path,
-        train_size=0.8,
-        random_state=wandb_config['random_state'],
-        stratify=list_class_name
-    )
-
-    return train_class_name, val_class_name, train_path_img, val_path_img
-
-
-def get_image_folder(config):
-    path = os.path.join(config['path']['data'], 'raw', 'train')
-    path = utils.get_notebooks_path(path)
-
-    return path
-
-
-def get_curated_class_path(config):
-    image_folder = get_image_folder(config)
-    curated_image_paths, curated_labels = utils.get_paths_labels(image_folder)
-    
-    return curated_image_paths, curated_labels
-
-
 def make_train_triplet_dataset(config, wandb_config):
-    curated_image_paths, curated_labels  = get_curated_class_path(config)
+    curated_image_paths, curated_labels = d_utils.get_curated_class_path(config)
     processor = dT.make_training_processor(config, wandb_config)
-    train_image_paths, _, train_labels, _ = get_train_val_split(wandb_config, curated_image_paths, curated_labels)
-    augmented_dataset = utils.load_augmented_dataset(wandb_config)
+    train_image_paths, _, train_labels, _ = d_utils.get_train_val_split(wandb_config, curated_image_paths, curated_labels)
+    augmented_dataset = d_utils.load_augmented_dataset(wandb_config)
     augmented_image_paths = [image_dict['image_path'] for image_dict in augmented_dataset]
     augmented_labels = [image_dict['label'] for image_dict in augmented_dataset]
     train_image_paths.extend(augmented_image_paths)
@@ -163,11 +119,11 @@ def make_train_triplet_dataset(config, wandb_config):
 
 
 def make_val_triplet_dataset(config, wandb_config):
-    curated_image_paths, curated_labels = get_curated_class_path(config)
+    curated_image_paths, curated_labels = d_utils.get_curated_class_path(config)
     processor = dT.make_training_processor(config, wandb_config)
-    _, val_class_name, _, val_path_img = get_train_val_split(wandb_config, list_class_name, list_img_path)
+    _, val_image_paths, _, val_labels = d_utils.get_train_val_split(wandb_config, curated_image_paths, curated_labels)
 
-    return RefConTripletDataset(wandb_config, val_class_name, val_path_img, processor, False)
+    return RefConTripletDataset(wandb_config, val_image_paths, val_labels, processor, False)
 
 
 def _debug():
@@ -178,13 +134,7 @@ def _debug():
     val_dataset = make_val_triplet_dataset(config, wandb_config)
     train_dataset = make_train_triplet_dataset(config, wandb_config)
 
-    for inputs in tqdm(val_dataset):
-        pass
-
-    for inputs in tqdm(train_dataset):
-        pass
-
-    pass
+    return
 
 
 if __name__ == '__main__':
